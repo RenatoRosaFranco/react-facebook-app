9ade958c0561db5ad6ecf51f7764087039b89912
Renato Rosa Franco <renato_ny@live.com>
2020-07-14 22:46:21 -0300

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <div id='brand'>
+        <h4>Facebook</h4>
+      </div>
+
+      <div id='nav'>
+        Meu Perfil
+      </div>
+    </header>
+  )
+}
+
+export default Header;
